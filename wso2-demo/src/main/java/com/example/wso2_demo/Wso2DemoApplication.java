e8cfb675442e9de52c4368c718b66a8b0fafe5a4
nannnilarlinn <nannnilarlinn@abank.com.mm>
2026-01-29 22:55:54 +0630

    wso2-demo

---
+++ wso2-demo/src/main/java/com/example/wso2_demo/Wso2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wso2_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wso2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wso2DemoApplication.class, args);
+	}
+
+}
---
+++ wso2-demo/src/test/java/com/example/wso2_demo/Wso2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wso2_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wso2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
